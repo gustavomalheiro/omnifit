080173f1fac490e56e65c9fd57b0b8a404696811
Gustavo Malheiro <gus.malheiro@gmail.com>
2026-03-30 22:42:34 -0300

    feat: first commit

---
+++ src/main/java/br/com/malheiro/tech/omnifit/OmnifitApplication.java
@@ -0,0 +1,13 @@
+package br.com.malheiro.tech.omnifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnifitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnifitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/malheiro/tech/omnifit/OmnifitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.malheiro.tech.omnifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnifitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
